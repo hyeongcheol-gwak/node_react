c698ca602d01ce1cc9e4ddcd49f7644219cd46b3
hyeongcheol-gwak <gwak0720@snu.ac.kr>
2023-04-05 13:51:45 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const config = require("./config/key");
+
+const bodyParser = require("body-parser");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://gwak0720:Rz00g63HirZIYnL7@node-react.okpnnoe.mongodb.net/test",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB is connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!zzzzzzzzzz");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then((userInfo) => {
+      return res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
