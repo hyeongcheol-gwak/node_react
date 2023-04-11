5081f3bf016124decf6bf888c6958404415c12d7
hyeongcheol-gwak <gwak0720@snu.ac.kr>
2023-04-11 16:40:19 +0900

    auth 추가

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const port = 5000;
 const config = require("./config/key");
 
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const { User } = require("./models/User");
 
+const { auth } = require("./middleware/auth");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -21,10 +25,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!zzzzzzzzzz");
+  res.send("Hello World!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
   user
     .save()
@@ -35,6 +39,58 @@ app.post("/register", (req, res) => {
     })
     .catch((err) => {
       return res.json({ success: false, err });
+    });
+});
+
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "제공된 이메일에 해당하는 유저가 없습니다.",
+        });
+      }
+
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) {
+          return res.json({
+            loginSuccess: false,
+            message: "비밀번호가 틀렸습니다",
+          });
+        }
+      });
+
+      user.generateToken((user, err) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" })
+    .then((user) => {
+      if (user) return res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      if (err) return res.json({ success: false, err });
     });
 });
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+var jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +17,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50,
+    maxlength: 500,
   },
   lastname: {
     type: String,
@@ -31,6 +36,51 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else next();
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save().then((user, err) => {
+    if (err) return cb(null, err);
+    cb(user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    user
+      .findOne({ _id: decoded, token: token })
+      .then((user) => {
+        if (user) return cb(null, user);
+      })
+      .catch((err) => {
+        if (err) return cb(err);
+      });
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
